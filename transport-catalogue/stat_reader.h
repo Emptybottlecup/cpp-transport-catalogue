1661e1cef285e6721f52a7e2e467d05ecc04f296
Emptybottlecup <116058449+Emptybottlecup@users.noreply.github.com>
2023-08-31 01:39:07 +0700

    Delete stat_reader.h

--- transport-catalogue/stat_reader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "input_reader.h"
-namespace transport_catalog {
-    namespace reader {
-void StatReader(transport_catalog::TransportCatalogue& catalog, std::istream& input);
-std::ostream& PrintBus(transport_catalog::TransportCatalogue& catalog, const std::string& bus, std::ostream& out);
-std::ostream& PrintStop(transport_catalog::TransportCatalogue& catalog, const std::string& stop, std::ostream& out);        
-    }
-}
